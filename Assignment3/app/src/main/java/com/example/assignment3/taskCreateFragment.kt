c4ff0d06c90d615fbad425769bb45bbe98b327be
ZakRepar <repar1@ottebein.edu>
2020-10-29 20:27:21 -0400

    add task

--- Assignment3/app/src/main/java/com/example/assignment3/taskCreateFragment.kt
+++ Assignment3/app/src/main/java/com/example/assignment3/taskCreateFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.assignment3.model.Task
+import kotlinx.android.synthetic.main.fragment_task_create.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,12 +29,14 @@ class taskCreateFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_create, container, false)
+        val view = inflater.inflate(R.layout.fragment_task_create, container, false)
+
+        view.findViewById<Button>(R.id.buttonSave).setOnClickListener {
+            onSaveCLick()
+        }
+        return view
     }
 
     companion object {
@@ -51,5 +57,34 @@ class taskCreateFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    } //end companion
+
+
+    private fun onSaveCLick() {
+
+        val description = editTextDescription.text.toString()
+
+        var emoji: String? = null
+        if (radioButtonCompleted.isChecked) emoji = radioButtonCompleted.text.toString()
+        if (radioButtonDoToday.isChecked) emoji = radioButtonDoToday.text.toString()
+        if (radioButtonUpcoming.isChecked) emoji = radioButtonUpcoming.text.toString()
+
+        var priority: String? = null
+        if (radioButtonHigh.isChecked) priority = radioButtonHigh.text.toString()
+        if (radioButtonMedium.isChecked) priority = radioButtonMedium.text.toString()
+        if (radioButtonLow.isChecked) priority = radioButtonLow.text.toString()
+
+        var state: String? = null
+        if (radioButtonOpened.isChecked) state = radioButtonOpened.text.toString()
+        if (radioButtonClosed.isChecked) state = radioButtonClosed.text.toString()
+
+        if (description != null) {
+            val task = Task(description, emoji, priority, state)
+            TaskEntryController.add(task)
+
+        }
+
+        findNavController().popBackStack()
     }
-}
+
+} // end class
