390f52343ccbc2b77822a707a3e20861f0c9b309
ZakRepar <repar1@ottebein.edu>
2020-11-08 02:28:59 -0500

    initial networking and layout

---
+++ MyApplication/app/src/main/java/comp4290/Assignment4/CryptoController.kt
@@ -0,0 +1,29 @@
+package comp4290.Assignment4
+
+class CryptoController {
+
+    var cryptoList : MutableList<Crypto> = mutableListOf<Crypto>()
+
+    val size: Int get() { return cryptoList.size }
+
+
+
+    fun cryptoAtIndex(index: Int) : Crypto? {
+
+        if ((index >= 0) && (index <= cryptoList.size)) {
+            return cryptoList[index]
+        }
+
+        return null
+    }
+
+
+
+    fun add(crypto: Crypto) {
+
+        if (cryptoList.find { it.id == crypto.id } == null) {
+            cryptoList.add(crypto)
+        }
+    }
+
+} // end class
