81380d2ba12de8425179bca47c35a81058ccfa1d
ZakRepar <repar1@ottebein.edu>
2020-11-14 14:05:36 -0500

    image handling

--- CryptoAssignment/app/src/main/java/com/example/cryptoassignment/ListFragment.kt
+++ CryptoAssignment/app/src/main/java/com/example/cryptoassignment/ListFragment.kt
@@ -7,22 +7,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoassignment.model.CoinController
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineExceptionHandler
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+const val ARG_ENTRY_INDEX = "entryIndex"
+
 class ListFragment : Fragment(), CoinRecyclerAdapter.CoinRecyclerAdapterDelegate {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    private var entryIndex: Int? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+       val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        if(entryIndex != null) {
+            val coin = CoinController.getCoinAtPosition(entryIndex!!)
+
+            view.findViewById<TextView>(R.id.textViewID).text = coin?.id.orEmpty()
+            view.findViewById<TextView>(R.id.textViewSymbol).text = coin?.symbol.orEmpty()
+            view.findViewById<TextView>(R.id.textViewPrice).text = coin?.price.toString()
+
+            Picasso.get().load(coin?.logo?.get(0)).placeholder(R.drawable.coin_default_detail_image).into(view.findViewById<ImageView>(R.id.imageViewLogo))
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- CryptoAssignment/app/src/main/java/com/example/cryptoassignment/model/Coin.kt
+++ CryptoAssignment/app/src/main/java/com/example/cryptoassignment/model/Coin.kt
@@ -2,7 +2,11 @@ package com.example.cryptoassignment.model
 
 class Coin (val logo: Image?, val id: String, val symbol: String?, val price: MarketData?)
 
-class Image(val size: List<String>?)
+class Image(val size: List<String>?) {
+    operator fun get(i: Int): String? {
+        return size?.get(i)
+    }
+}
 
 class MarketData(val prices: List<CurrentPrice>?)
 
